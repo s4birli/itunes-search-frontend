8699412e0dbe3bd92887fe5444db77023d1b7a08
Mehmet Sabırlı <sabirli31@gmail.com>
2022-09-12 17:36:18 +0100

    Some tests for the search component

--- src/components/Search/Search.test.tsx
+++ src/components/Search/Search.test.tsx
@@ -1,16 +1,34 @@
-import { render } from '../../test-utils';
+import userEvent from '@testing-library/user-event';
+
+import { render, fireEvent } from '../../test-utils';
 
 import Search from './Search';
 
 describe('<Search />', () => {
   it('should render', () => {
-    const { container } = render(
-      <Search
-        onKeyDown={function (): void {
-          throw new Error('Function not implemented.');
-        }}
-      />
-    );
+    const { container } = render(<Search />);
+
     expect(container).toMatchSnapshot();
+  });
+
+  it('should implement the onChange event', async () => {
+    const onChange = jest.fn();
+    const value = 'Test Value';
+
+    const { getByRole } = render(<Search onChange={onChange} />);
+    await userEvent.click(getByRole('textbox'));
+    await userEvent.paste(value);
+
+    expect(onChange).toBeCalledTimes(1);
+  });
+
+  it('should implement the onKeydown event', async () => {
+    const onKeydown = jest.fn();
+
+    const { getByRole } = render(<Search onKeyDown={onKeydown} />);
+    await userEvent.click(getByRole('textbox'));
+    fireEvent.keyDown(getByRole('textbox'), new KeyboardEvent('keydown', { key: 'Enter', code: 'Enter', keyCode: 13 }));
+
+    expect(onKeydown).toBeCalledTimes(1);
   });
 });
